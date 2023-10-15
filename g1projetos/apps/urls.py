c32c447cfab1ebbe44668144d40f0dbbaa316ac7
Gabrielbzandrade <gba@cesar.school>
2023-10-15 19:29:52 -0300

    Commit 2

--- g1projetos/apps/urls.py
+++ g1projetos/apps/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.inicio, name="inicio"),
+    path('transparencia/', views.transparencia, name="transparencia"),
+    path('portalpadrinhos/', views.portalpadrinhos, name="portalpadrinhos"),
+    path('portalfuncionarios/', views.portalfuncionarios, name="portalfuncionarios"),
 ]
--- g1projetos/apps/views.py
+++ g1projetos/apps/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 
 def inicio(request):
-    return render(request, 'apps/inicio.html')
+    return render(request, 'apps/baseprincipal.html')
+
+def transparencia(request):
+    return render(request, 'apps/transparencia.html')
+
+def portalpadrinhos(request):
+    return render(request, 'apps/portalpadrinhos.html')
+
+def portalfuncionarios(request):
+    return render(request, 'apps/portalfuncionarios.html')
